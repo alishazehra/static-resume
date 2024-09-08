f8ed2f7a730d617a95ffb143c068d07ae7877430
alisha zehra <alishazehra866@gmail.com>
2024-09-08 12:35:39 +0500

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 var _a;
-(_a = document.getElementById('submitButton')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
-    alert('Form submitted');
+// Static resume builder in which only buttons work other information remains unchanged
+(_a = document.getElementById("submitButton")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+    alert("Resume updated");
+});
+// script.ts
+document.addEventListener("DOMContentLoaded", function () {
+    var toggleSkillsBtn = document.getElementById("toggleSkillsBtn");
+    var skillsSection = document.getElementById("skillsSection");
+    toggleSkillsBtn.addEventListener("click", function () {
+        if (skillsSection.classList.contains("hidden")) {
+            skillsSection.classList.remove("hidden");
+        }
+        else {
+            skillsSection.classList.add("hidden");
+        }
+    });
 });
--- script.ts
+++ script.ts
@@ -1,5 +1,22 @@
-document.getElementById('submitButton')?.addEventListener('click', () => {
-    alert('Resume updated');
+// Static resume builder in which only buttons work other information remains unchanged
+document.getElementById("submitButton")?.addEventListener("click", () => {
+  alert("Resume updated");
 });
 
+// script.ts
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleSkillsBtn = document.getElementById(
+    "toggleSkillsBtn"
+  ) as HTMLButtonElement;
+  const skillsSection = document.getElementById(
+    "skillsSection"
+  ) as HTMLDivElement;
 
+  toggleSkillsBtn.addEventListener("click", () => {
+    if (skillsSection.classList.contains("hidden")) {
+      skillsSection.classList.remove("hidden");
+    } else {
+      skillsSection.classList.add("hidden");
+    }
+  });
+});
