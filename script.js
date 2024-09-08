24e538d7cd1c1e3b7e5d6985f7f6efeb1128b8ff
alisha zehra <alishazehra866@gmail.com>
2024-09-08 07:30:49 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var _a;
+(_a = document.getElementById('submitButton')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
+    alert('Form submitted');
+});
---
+++ script.ts
@@ -0,0 +1,5 @@
+document.getElementById('submitButton')?.addEventListener('click', () => {
+    alert('Resume updated');
+});
+
+
